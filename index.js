6d7b67cd179b95b6c0e8b5d502d3b800ac703bea
Grace Shih <vocalise@gmail.com>
2020-03-08 05:10:44 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -13,11 +13,23 @@ class Circle {
     return this.radius*2;
   }
   
+  set diameter(value) {
+    this.radius = value / 2;
+  }
+  
   get circumference() {
     return this.radius*2 * Math.PI;
   }
   
+  set circumference(value) {
+    this.radius = value / Math.PI / 2;  
+  }
+  
   get area() {
     return Math.PI * this.radius * this.radius;
+  }
+  
+  set area(value) {
+    this.radius = value / Math.PI / this.radius;
   }
 }
